6ec5a69b2dcebffad245bdf343fd50fc21d3537c
rizwanbutt1158-dev <rizwanbutt1158@gmail.com>
2025-10-22 17:03:56 +0500

    Task1

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+import { View, Text,Button } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View> // view ma wrap kar la ga 
+      <Text style ={{ fontSize : 50 }}>RizwanIlyasButt</Text>
+      <Text style ={{ fontSize : 50 }}>Devops</Text>
+      <Button title = "Press Here"/>
+    </View>
+  )
+}
+
+export default App
