38c681400ce666fc5328ba5589fc743272195db1
rizwanbutt1158-dev <rizwanbutt1158@gmail.com>
2025-10-22 17:37:27 +0500

    Task2src/componenetsfilelink

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 
 import { View, Text,Button } from 'react-native'
-import React from 'react'
+import React from 'react';
+import First from './src/components/First';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Text style ={{ fontSize : 50 }}>RizwanIlyasButt</Text>
       <Text style ={{ fontSize : 50 }}>Devops</Text>
       <Button title = "Press Here"/>
+      <First />
     </View>
   )
 }
---
+++ src/components/First.jsx
@@ -0,0 +1,13 @@
+import { View, Text,Button } from 'react-native'
+import React from 'react'
+
+
+const Test = () => {
+  return (
+    <View> // view ma wrap kar la ga 
+      <Text style ={{ fontSize : 50 }}>RLTSQUARE</Text>
+      <Text style ={{ fontSize : 50 }}>software house</Text>
+    </View>
+  )
+}
+export default Test
